e13b7f4d47ddebc76f652f6cff69590653a7b652
Archie George <archiegeorge23@gmail.com>
2025-03-13 13:43:44 +0000

    gsap removal

--- src/components/HomeLogo.js
+++ src/components/HomeLogo.js
@@ -1,30 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LogoS from "../assets/images/LogoS.png";
-import { gsap } from 'gsap-trial'
-import { Draggable } from 'gsap-trial/Draggable'
-import { InertiaPlugin } from 'gsap-trial/InertiaPlugin';
 
 const HomeLogo = () => {
-
-    const resetLogo = () => {
-        const currentRotation = gsap.getProperty(".logo-img", "rotation");
-
-        gsap.to(".logo-img", {
-          rotation: currentRotation,
-          x: 0,
-          y: 0,
-          duration: 1,
-        });
-      };
-
-      useEffect(() => {
-        gsap.registerPlugin(Draggable, InertiaPlugin);
-
-        Draggable.create(".logo-img", {
-          type: "rotation",
-          inertia: true,
-        });
-      }, []);
 
     return (
         <div className="logo-container" onClick={resetLogo}>
