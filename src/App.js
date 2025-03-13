16b8413da456562deef4e18b4998c81e980abc68
Archie George <archiegeorge23@gmail.com>
2025-03-13 14:11:25 +0000

    favicon change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import About from './components/About';
-import PortfolioPage from './components/pages/portfolio';
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import PortfolioPage from './components/pages/portfolio'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="About" element={<About/>}/>
-        <Route path="portfolio" element={<PortfolioPage/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="portfolio" element={<PortfolioPage />} />
+        </Route>
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
